881610b876773bb644eac492d3d4409dc254ed67
Stephen McGowan <smcgowan@oneviewhealthcare.com>
2017-07-03 21:30:48 +1000

    Initial commit, requires x86 powershell currently.

---
+++ ProcessAip.ps1
@@ -0,0 +1,45 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [String]$inputAIPFile
+    )
+
+function Create-ScriptEngine()
+{
+  param([string]$language = $null, [string]$code = $null);
+  if ( $language )
+  {
+    $sc = New-Object -ComObject ScriptControl;
+    $sc.Language = $language;
+    if ( $code )
+    {
+      $sc.AddCode($code);
+    }
+    $sc.CodeObject;
+  }
+}
+
+$jsCode = 
+@"
+function getChecksum(l) {
+        var k = 0;
+        if (l.length == 0) {
+            return k
+        }
+        for (var j = 0; j < l.length; j++) {
+            var h = l.charCodeAt(j);
+            k = ((k << 5) - k) + h;
+            k = k & 4294967295
+        }
+        if (k < 0) {
+            k = ~k
+        }
+        return k
+    };
+"@
+
+$js = Create-ScriptEngine "JScript" $jsCode
+
+[xml]$xml = Get-Content -Path $inputAIPFile
+$colorsStr = [string]$xml.ramp.colors.OuterXml
+
+$js.getChecksum($colorsStr)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AquaIlluminationTools
 Tools for interacting with AquaIllumination lights
+
+WARNING: These tools are supplied as is. They could and probably do violate the warranty of your AquaIllumination lights. These tools have only been tested with the following hardware and software version. If anyone else test this with other
+
+Hydra 26 HD - 1.4.3-227
