5176960d51b2ae153ddb5523ba9e23848bb3871e
Stephen McGowan <smcgowan@oneviewhealthcare.com>
2017-07-24 23:08:03 +1000

    Adding script switches, to allow checksum calculation and updating.

--- ProcessAip.ps1
+++ ProcessAip.ps1
@@ -1,12 +1,34 @@
 ﻿param(
-    [Parameter(Mandatory=$true)]
-    [String]$inputAIPFile
+    [Parameter(Mandatory=$true, Position=1)]
+    [String]$inFile,
+    [Parameter(Position=2)]
+    [String]$outFile = $null,
+    [switch]$calculateChecksum,
+    [switch]$updateChecksum
     )
 
-function Get-Checksum()
-{
-    param([String]$checkStr = $null)
+function Format-XML
+{ 
+    param([xml]$xml, $indentChar="`t", $indent=1) 
+    
+    $StringWriter = New-Object System.IO.StringWriter 
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter 
+    $xmlWriter.Formatting = “indented” 
+    $xmlWriter.Indentation = $indent 
+    $xmlWriter.IndentChar = $indentChar
+    $xml.WriteContentTo($XmlWriter) 
+    $XmlWriter.Flush() 
+    $StringWriter.Flush() 
+    
+    return $StringWriter.ToString() 
+}
 
+function Get-Checksum
+{
+    param([xml]$xml)
+
+    $checkstr = [string]$xml.ramp.colors.OuterXml
+    
     $x= 0
 
     for($i = 0; $i -lt $checkStr.Length; $i++)
@@ -23,7 +45,28 @@ function Get-Checksum()
     return [Math]::Abs($x) 
 }
 
-[xml]$xml = Get-Content -Path $inputAIPFile
-$colorsStr = [string]$xml.ramp.colors.OuterXml
+function Modify-Checksum
+{
+    param([xml]$xml)
 
-Get-Checksum $colorsStr
+    $checksum = Get-Checksum($xml)
+    $xml.ramp.header.checksum = ""+$checksum
+    return $xml
+}
+
+<#
+    AIP File Processing
+#>
+
+[xml]$xml = Get-Content -Path (Resolve-Path $inFile)
+
+if( $calculateChecksum )
+{
+    Write-Output (Get-Checksum($xml))
+}
+
+if( $updateChecksum )
+{
+    $outXML = Modify-Checksum($xml)
+    Format-XML($outXML) | Out-File -FilePath $outFile
+}
